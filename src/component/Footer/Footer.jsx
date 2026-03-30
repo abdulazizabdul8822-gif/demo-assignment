1925b0cb4e88647307b6e94ccdea0468a0c84bf0
Md Azizul Islam <mdazizulislam@Mds-MacBook-Air.local>
2026-03-30 16:19:59 +0600

    modefied footer section

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FaInstagram, FaXTwitter, FaFacebookSquare, } from "react-icons/fa";
+import { FaInstagram, FaFacebookSquare } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 
 const Footer = () => {
